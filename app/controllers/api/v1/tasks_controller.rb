b9cf6e11ec9cd827b17c80a1c18a01eafd760292
BigBinary <bigbinary@Sarthaks-Macbook-Pro.local>
2025-02-28 14:40:01 +0530

    Added ability to update a task

--- app/controllers/api/v1/tasks_controller.rb
+++ app/controllers/api/v1/tasks_controller.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class Api::V1::TasksController < ApplicationController
+  before_action :load_task!, only: %i[show update]
   def index
     tasks = Task.all
     Rails.logger.debug "Tasks found: #{tasks.count}"
@@ -14,11 +15,19 @@ class Api::V1::TasksController < ApplicationController
   end
 
   def show
-    task = Task.find_by!(slug: params[:slug])
     render_json({ task: task })
   end
 
+  def update
+    task.update!(task_params)
+    render_notice(t("successfully_updated"))
+  end
+
   private
+
+    def load_task!
+      @task = Task.find_by!(slug: params[:slug])
+    end
 
     def task_params
       params.require(:task).permit(:title)
