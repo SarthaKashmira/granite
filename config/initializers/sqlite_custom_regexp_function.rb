5c22b59631d2b0f797c78e5a8328ad81e8f7c56d
BigBinary <bigbinary@Sarthaks-Macbook-Pro.local>
2025-03-23 14:21:26 +0530

    Fixed issues regarding production

--- config/initializers/sqlite_custom_regexp_function.rb
+++ config/initializers/sqlite_custom_regexp_function.rb
@@ -3,14 +3,16 @@
 ActiveRecord::ConnectionAdapters::SQLite3Adapter.class_eval do
   alias_method :orig_initialize, :initialize
 
-  def initialize(*args)
-    orig_initialize(*args)
+  if defined?(ActiveRecord::ConnectionAdapters::SQLite3Adapter)
+    def initialize(*args)
+      orig_initialize(*args)
 
-    # is_sqlite_db = ActiveRecord::Base.connection_db_config.configuration_hash[:adapter] == "sqlite3"
+      # is_sqlite_db = ActiveRecord::Base.connection_db_config.configuration_hash[:adapter] == "sqlite3"
 
-    raw_connection.create_function("regexp", 2) do |fn, pattern, expr|
-      regex_matcher = Regexp.new(pattern.to_s, Regexp::IGNORECASE)
-      fn.result = expr.to_s.match(regex_matcher) ? 1 : 0
+      raw_connection.create_function("regexp", 2) do |fn, pattern, expr|
+        regex_matcher = Regexp.new(pattern.to_s, Regexp::IGNORECASE)
+        fn.result = expr.to_s.match(regex_matcher) ? 1 : 0
+      end
     end
   end
 end
